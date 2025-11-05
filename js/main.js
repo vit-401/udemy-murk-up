ce18196e60bad2760afff41e557cd781d90714e6
vitaliiprysiazhniuk <vetastektonik1@gmail.com>
2025-11-04 21:13:59 -0800

    Add responsive hamburger menu; implement toggle functionality and styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(function(){
+    // Mobile menu toggle
+    $('.menu__burger').on('click', function(){
+        $(this).toggleClass('active');
+        $('.menu').toggleClass('active');
+    });
 
+    // Close menu when clicking outside
+    $(document).on('click', function(e){
+        if (!$(e.target).closest('.menu, .menu__burger').length) {
+            $('.menu__burger').removeClass('active');
+            $('.menu').removeClass('active');
+        }
+    });
+
+    // Close menu when clicking a menu link
+    $('.menu__link').on('click', function(){
+        $('.menu__burger').removeClass('active');
+        $('.menu').removeClass('active');
+    });
 });
